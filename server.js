96c172f8e379a82a1f8103e47c7d3cd8c6c193b2
cshipley98 <claireshipley98@gmail.com>
2024-03-26 10:18:57 -0500

    express.url

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded ( {  extended: true }));
